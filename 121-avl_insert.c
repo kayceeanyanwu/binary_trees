f492f83393d66cd4d980ee8b713d4640991b7968
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-09-22 21:35:26 +0530

    added task 121

---
+++ 121-avl_insert.c
@@ -0,0 +1,66 @@
+#include "binary_trees.h"
+
+avl_t *avli_helper(avl_t **tree, avl_t *parent, avl_t **new, int value);
+
+/**
+ * avl_insert - inserts a value in an AVL Tree
+ * @tree: double pointer to the root node of the AVL tree for inserting value
+ * @value: value to store in the node to be inserted
+ *
+ * Return: a pointer to the created node, or NULL if failed
+ */
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	avl_t *new = NULL;
+
+	if (!tree)
+		return (NULL);
+	if (!(*tree))
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+	avli_helper(tree, *tree, &new, value);
+	return (new);
+}
+
+avl_t *avli_helper(avl_t **tree, avl_t *parent, avl_t **new, int value)
+{
+	int bf;
+
+	if (*tree == NULL)
+		return (*new = binary_tree_node(parent, value));
+
+	if ((*tree)->n > value)
+	{
+		(*tree)->left = avli_helper(&(*tree)->left, *tree, new, value);
+		if (!(*tree)->left)
+			return (NULL);
+	}
+	else if ((*tree)->n < value)
+	{
+		(*tree)->right = avli_helper(&(*tree)->right, *tree, new, value);
+		if (!(*tree)->right)
+			return (NULL);
+	}
+	else
+		return (*tree);
+
+	bf = binary_tree_balance(*tree);
+	if (bf > 1 && (*tree)->left->n > value)
+		*tree = binary_tree_rotate_right(*tree);
+	else if (bf < -1 && (*tree)->right->n < value)
+		*tree = binary_tree_rotate_left(*tree);
+	else if (bf > 1 && (*tree)->left->n < value)
+	{
+		(*tree)->left = binary_tree_rotate_left((*tree)->left);
+		*tree = binary_tree_rotate_right(*tree);
+	}
+	else if (bf < -1 && (*tree)->right->n > value)
+	{
+		(*tree)->right = binary_tree_rotate_left((*tree)->right);
+		*tree = binary_tree_rotate_left(*tree);
+	}
+
+	return (*tree);
+}
